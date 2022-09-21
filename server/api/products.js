f07787540b8b6defd0c4408948352229fbd4b84c
Joyce-Wong <joycekwo12@gmail.com>
2022-09-21 16:47:45 -0400

    before merge to main

--- server/api/products.js
+++ server/api/products.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const {
   models: { Product },
 } = require("../db");
+const { requireToken, isAdmin } = require("./gatekeepingMiddleware");
 module.exports = router;
 
 router.get("/", async (req, res, next) => {
@@ -19,6 +20,23 @@ router.get("/:id", async (req, res, next) => {
     const singleProduct = await Product.findByPk(req.params.id);
     res.send(singleProduct);
   } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", requireToken, isAdmin, async (req, res, next) => {
+  try{
+    res.status(201).send(await Product.create(req.body));
+  } catch(error){
+    next(error);
+  }
+});
+
+router.put("/:id", requireToken, isAdmin, async(req, res, next) => {
+  try{
+    const product = await Product.findByPk(req.params.id)
+    res.send(await product.update(req.body))
+  } catch(error){
     next(error);
   }
 });

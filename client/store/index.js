5f6134b0cb6172ee7ec171b47521c9f1a4e7a257
Ashley Quevedo <ashleysquevedo@gmail.com>
2022-09-21 14:36:39 -0400

    added single products GET route and component

--- client/store/index.js
+++ client/store/index.js
@@ -1,16 +1,20 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
-import productsReducer from './products'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import auth from "./auth";
+import productsReducer from "./products";
+import singleProductReducer from "./redux/singleProduct";
 
-const reducer = combineReducers({ auth, 
-  products: productsReducer })
+const reducer = combineReducers({
+  auth,
+  products: productsReducer,
+  product: singleProductReducer,
+});
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
+export default store;
+export * from "./auth";

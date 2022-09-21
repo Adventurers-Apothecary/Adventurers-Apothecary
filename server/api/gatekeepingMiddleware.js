6b87ccee6fa7048545d8e7ea3b12dc7c7b11e6b9
Joyce-Wong <joycekwo12@gmail.com>
2022-09-21 14:48:21 -0400

    only admins can see all users

---
+++ server/api/gatekeepingMiddleware.js
@@ -0,0 +1,20 @@
+// store all of our functions that will act as 
+// middleware between our request and our response
+// and we will use it as we see fit
+
+const { models: { User }} = require('../db')
+
+const requireToken = async(req, res, next) => {
+    try {
+        const token = req.headers.authorization
+        const user = await User.findByToken(token)
+        req.user = user
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    requireToken
+}
--- server/api/users.js
+++ server/api/users.js
@@ -1,9 +1,13 @@
 const router = require('express').Router()
 const { models: { User }} = require('../db')
+const {requireToken} = require('./gatekeepingMiddleware')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', requireToken, async (req, res, next) => {
   try {
+    if(!req.user.isAdmin){
+      return res.status(403).send('Only admins may see all user information.')
+    }
     const users = await User.findAll({
       // explicitly select only the id and username fields - even though
       // users' passwords are encrypted, it won't help if we just

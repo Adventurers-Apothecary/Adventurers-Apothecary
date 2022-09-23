3bbfa1c0ae2d4abae6dc19ee7c3f17b8c6607a5d
Ashley Quevedo <ashleysquevedo@gmail.com>
2022-09-23 15:46:54 -0400

    resolved some git issues

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -1,28 +1,31 @@
-// store all of our functions that will act as 
+// store all of our functions that will act as
 // middleware between our request and our response
 // and we will use it as we see fit
 
-const { models: { User }} = require('../db')
+const {
+  models: { User },
+} = require("../db");
 
-const requireToken = async(req, res, next) => {
-    try {
-        const token = req.headers.authorization
-        const user = await User.findByToken(token)
-        req.user = user
-        next()
-    } catch (error) {
-        next(error)
-    }
-}
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token);
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
 
 const isAdmin = (req, res, next) => {
-    if(!req.user.isAdmin){
-        return res.status(403).send('Only admins may perform this function.')
-      } else{
-          next()
-      }
-}
+  if (!req.user.isAdmin) {
+    return res.status(403).send("Only admins may perform this function.");
+  } else {
+    next();
+  }
+};
+
 module.exports = {
-    requireToken,
-    isAdmin
-}
+  requireToken,
+  isAdmin,
+};
